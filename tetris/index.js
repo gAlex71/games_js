873505a408e0592e31b3b5528cdfc32718b8f93f
gAlex71 <alexvladimirovich71@mail.ru>
2024-01-29 23:55:02 +0300

    draw first figure

--- tetris/index.js
+++ tetris/index.js
@@ -1,3 +1,4 @@
+import { convertPositionToIndex } from "./helpers.js";
 import { Tetris } from "./tetris.js";
 
 const gridElement = document.querySelector('.grid');
@@ -8,3 +9,30 @@ for(let i = 0; i < 200; i++){
 }
 
 const tetris = new Tetris();
+//Получаем все ячейки поля
+const cells = document.querySelectorAll('.grid>div');
+
+draw();
+
+function draw() {
+    //Очищаем от всех стилей ячейки
+    cells.forEach(cell => cell.removeAttribute('class'));
+
+    drawTetramino();
+};
+
+function drawTetramino() {
+    const name = tetris.tetromino.name;
+    const tetrominoMatrixSize = tetris.tetromino.matrix.length;
+
+    for(let row = 0; row < tetrominoMatrixSize; row++){
+        for(let column = 0; column < tetrominoMatrixSize; column++){
+            if (!tetris.tetromino.matrix[row][column]) continue;
+            if (tetris.tetromino.row + row < 0) continue;
+            //Пересчитываем индекс из матрицы в индекс div элементов
+            const cellIndex = convertPositionToIndex(tetris.tetromino.row + row, tetris.tetromino.column + column);
+            //Добавляем к div класс с именем фигуры
+            cells[cellIndex].classList.add(name);
+        }
+    }
+}
--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,4 +1,4 @@
-import { PLAYFIELD_COLUMNS, PLAYFIELD_ROWS } from "./helpers.js";
+import { PLAYFIELD_COLUMNS, PLAYFIELD_ROWS, TETROMINOES, TETROMINONAMES, getRandomElement } from "./helpers.js";
 
 export class Tetris {
     constructor() {
@@ -9,6 +9,7 @@ export class Tetris {
 
     init() {
         this.generatePlayField();
+        this.generateTetromino();
     }
 
     generatePlayField() {
@@ -16,5 +17,21 @@ export class Tetris {
         this.playfield = new Array(PLAYFIELD_ROWS).fill().map(() => new Array(PLAYFIELD_COLUMNS).fill(0));
 
         console.table(this.playfield);
+    }
+
+    generateTetromino() {
+        const name = getRandomElement(TETROMINONAMES);
+        const matrix = TETROMINOES[name];
+
+        const column = PLAYFIELD_COLUMNS / 2 - Math.floor(matrix.length / 2);
+        // const row = -2;
+        const row = 3;
+
+        this.tetromino = {
+            name,
+            matrix,
+            column,
+            row
+        }
     }
 }
