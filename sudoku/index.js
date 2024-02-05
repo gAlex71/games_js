9f4ae97003fd5e215c7a8a4455579959b675f783
gAlex71 <alexvladimirovich71@mail.ru>
2024-02-06 00:16:28 +0300

    add styles

--- sudoku/index.js
+++ sudoku/index.js
@@ -1,3 +1,11 @@
 import { Sudoku } from "./sudoku.js";
 
+const gridElement = document.querySelector('.grid');
+
+for(let i = 0; i < 81; i++){
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('cell');
+    gridElement.appendChild(newDiv);
+}
+
 const sudoku = new Sudoku();
